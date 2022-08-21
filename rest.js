28f080eb7d3a52cced82179eadecb6edf8b3e459
beryl-ajuoga <berrylajuoga@gmail.com>
2022-08-21 23:58:50 +0300

    change

--- rest.js
+++ rest.js
@@ -1,25 +1,62 @@
-let listProducts =document.getElementById("product")
-listProducts.firstElementChild('/products' , {
-    method:"get" ,
-    Headers:{
-        "Content-type":"application/json"
-    }
+fetch('http://localhost:5000/products')
+.then(function(response) {
+return response.json();
 })
+.then(function(product) {
+console.log(product);
 
-let listVegetables=document.getElementById("veglist")
-listVegetables.firstElementChild('/veglist' ,{
-    method:"get" ,
-    Headers:{
-        "Content-type":"application/json"
-    }
+let listProducts = document.getElementById('products')
+product.forEach(a => {
+let myListProducts = document.createElement('li')
+myListProducts.innerText = `${a.name}`
+listProducts.appendChild(myListProducts)
 })
+})
+.catch(function(error){
+console.log('error',error);
+});
 
-let listFruits=document.getElementById("fruList")
-listFruits.firstElementChild('/fruList' ,{
-    method:"get",
-    Headers:{
-        "content-type":"application/json"
-    }
+
+
+fetch('http://localhost:5000/"/products/fruits"')
+.then(function(response) {
+return response.json();
 })
+.then(function(fruitsss) {
+console.log(fruitsss);
+
+let listFruits = document.getElementById('fruiList')
+fruits.forEach(a => {
+let myListFruits = document.createElement('li')
+myListFruits.innerText = `${a.name}`
+listFruits.appendChild(li)
+})
+})
+.catch(function(error){
+console.log('error',error);
+});
+
+
+fetch('http://localhost:5000//products/vegetables')
+.then(function(response) {
+return response.json();
+})
+.then(function(vegetabless) {
+console.log(vegetabless);
+
+let listVeg = document.getElementById('veglist')
+product.forEach(a => {
+let myListVeg = document.createElement('li')
+myListVeg.innerText = `${a.name}`
+listVeg.appendChild(li)
+})
+})
+.catch(function(error){
+console.log('error',error);
+});
+
+
+
+
 
 
